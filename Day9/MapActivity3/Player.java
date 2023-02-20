0d4953723c3242ec2b7d820de16bbc6d61fbfc4c
Ishika Nimade <ishika.nimade@einfochips.com>
2023-02-20 07:36:36 +0530

    day 9 activity updated

---
+++ Day9/MapActivity3/Player.java
@@ -0,0 +1,16 @@
+public class Player {
+	
+	private String name;
+	private String team; 
+	private String skill;
+	public Player(String name, String team, String skill) {
+		this.name = name;
+		this.team = team;
+		this.skill = skill;
+	}
+	
+	public String toString() {
+		return name + "--" + team + "--" + skill;
+	}
+	
+}
