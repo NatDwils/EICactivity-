68e15b593c7ad084c1becdb0cb12c3ce22ed09d7
Ishika Nimade <ishika.nimade@einfochips.com>
2023-02-20 07:53:05 +0530

    day 10 sample assignment updated

---
+++ Day10/Question3/PowerRatingComparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class PowerRatingComparator implements Comparator<Player> {
+
+	@Override
+	public int compare(Player o1, Player o2) {
+		// TODO Auto-generated method stub
+		return Double.compare(o1.getPowerRating(), o2.getPowerRating());
+		
+	}
+
+}
