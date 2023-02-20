1e029494d2d876dc308fe334f5417445cb63de74
Ishika Nimade <ishika.nimade@einfochips.com>
2023-02-20 07:27:27 +0530

    day 8 activity updated

---
+++ Day8/ComparatorActivity1/PlayerComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class PlayerComparator implements Comparator<Player> {
+
+	    @Override
+	    public int compare(Player player1, Player player2) {
+	        int skillComparison = player1.getSkill().compareTo(player2.getSkill());
+	        if (skillComparison != 0) {
+	            return skillComparison;
+	        }
+	        return player1.getName().compareTo(player2.getName());
+	    }
+	}
